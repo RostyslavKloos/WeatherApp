c0b2b1d6feeccc019bdfb6e306e56d5224ffbd19
RostyslavKloos <rotsislavkloos@gmail.com>
2020-10-25 18:38:37 +0300

    app photo added

--- app/src/main/java/com/example/weatherapp/Application.kt
+++ app/src/main/java/com/example/weatherapp/Application.kt
@@ -3,7 +3,6 @@ package com.example.weatherapp
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
-import javax.inject.Inject
 
 @HiltAndroidApp
 open class Application : Application() {
